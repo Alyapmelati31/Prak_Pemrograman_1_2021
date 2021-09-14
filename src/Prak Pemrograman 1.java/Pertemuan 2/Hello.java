583d8f1d09f947a1912c071d51c1a8b3a7381bca
Melati Alya Putri Irawan <melatialya.2021@student.uny.ac.id>
2021-09-14 13:15:02 +0700

    adding comments

--- src/Prak Pemrograman 1.java/Pertemuan 2/Hello.java
+++ src/Prak Pemrograman 1.java/Pertemuan 2/Hello.java
@@ -1,6 +1,10 @@
+/*
+NIM : 21537144007
+Nama : Melati Alya Putri Irawan
+*/
 public class Hello {
     public static void main(String[] args) {
+        // untuk menampilkan teks
         System.out.println("Hello world");
     }    
 }
-
