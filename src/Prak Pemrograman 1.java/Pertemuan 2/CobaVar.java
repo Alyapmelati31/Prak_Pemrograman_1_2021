8c044fa2a6ab0459634d0a7d0f3c71ff881a535c
Melati Alya Putri Irawan <melatialya.2021@student.uny.ac.id>
2021-09-14 13:43:03 +0700

    Adding String

---
+++ src/Prak Pemrograman 1.java/Pertemuan 2/CobaVar.java
@@ -0,0 +1,10 @@
+public class CobaVar {
+    public static void main(String[] args) {
+        String namaPanggilan = "Alya";
+        Int umur = 18;
+        double tinggiBadan = 162;
+        System.out.println("Nama Saya" + namaPanggilan);
+        System.out.println("Umur Saya" + umur);
+        System.out.println("Tinggi badan Saya" + tinggiBadan);
+    }
+}
