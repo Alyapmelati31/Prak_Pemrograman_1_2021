9b7d2351d50f8eb20982b6e785c13f9b65a9f4e0
Melati Alya Putri Irawan <melatialya.2021@student.uny.ac.id>
2021-09-14 14:14:24 +0700

    cobavarian identitas

--- src/Prak Pemrograman 1.java/Pertemuan 2/CobaVar.java
+++ src/Prak Pemrograman 1.java/Pertemuan 2/CobaVar.java
@@ -1,10 +1,11 @@
 public class CobaVar {
     public static void main(String[] args) {
         String namaPanggilan = "Alya";
-        Int umur = 18;
+        int umur = 18 ;
         double tinggiBadan = 162;
-        System.out.println("Nama Saya" + namaPanggilan);
-        System.out.println("Umur Saya" + umur);
-        System.out.println("Tinggi badan Saya" + tinggiBadan);
+        System.out.println("Nama Saya " + namaPanggilan);
+        System.out.println("Umur Saya " + umur);
+        System.out.println("Tinggi badan Saya " + (int) tinggiBadan);
+        
     }
 }
