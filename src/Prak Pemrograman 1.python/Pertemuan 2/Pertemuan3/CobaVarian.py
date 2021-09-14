da4bfe9227150a2e5ed4a7c9994198362e33c58a
Melati Alya Putri Irawan <melatialya.2021@student.uny.ac.id>
2021-09-14 15:30:24 +0700

    Tugas Pertemuan3

--- src/Prak Pemrograman 1.python/Pertemuan 2/Pertemuan3/CobaVarian.py
+++ src/Prak Pemrograman 1.python/Pertemuan 2/Pertemuan3/CobaVarian.py
@@ -1,8 +1,8 @@
-namaPanggilan = "Alya"
+namaPanggilan = " Alya"
 umur = 18
 tinggiBadan = 1.6
-print(""Nama saya adalah " + namaPanggilan)
-print(""Umur saya " + str(umur) + "" tahun")
+print("Nama saya adalah " + namaPanggilan)
+print("Umur saya " + str(umur) + " tahun")
 print(tinggiBadan)
 print(type(namaPanggilan))
 print(type(umur))
