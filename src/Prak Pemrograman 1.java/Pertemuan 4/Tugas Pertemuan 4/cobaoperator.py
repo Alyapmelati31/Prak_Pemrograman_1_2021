4dd65c809de7c5639590128812f88d7e86b1d794
Melati Alya Putri Irawan <melatialya.2021@student.uny.ac.id>
2021-09-23 10:19:59 +0700

    Tugas_Pertemuan_4

---
+++ src/Prak Pemrograman 1.java/Pertemuan 4/Tugas Pertemuan 4/cobaoperator.py
@@ -0,0 +1,18 @@
+x = 10
+y = 6
+hasil = x % y
+print("hasil = " + str(hasil))
+
+a = 10
+b = 6
+nilai = a // b
+print("hasil = " + str(nilai))
+
+z = 1
+z +=1
+print(z)
+
+p = 7
+q = 9
+hasilnya = p + q
+print("hasil = " + str(hasilnya))
---
+++ src/Prak Pemrograman 1.java/Pertemuan 4/Tugas Pertemuan 4/tugas4.py
@@ -0,0 +1,19 @@
+x = input("Masukan nilai x = ")
+y = input("Masukan nilai y = ")
+
+hasil = int(x) + int(y)
+print("hasil = " + str(hasil))
+
+hasil = int(x) - int(y)
+print("hasil = " + str(hasil))
+
+hasil = int(x) * int(y)
+print("hasil = " + str(hasil))
+
+hasil = int(x) / int(y)
+print("hasil = " + str(hasil))
+
+hasil = int(x) % int(y)
+print("hasil = " + str(hasil))
+
+
