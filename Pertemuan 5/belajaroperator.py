0e6c62bd2790e0f70ba33cb4d15ff6b32057db94
Melati Alya Putri Irawan <melatialya.2021@student.uny.ac.id>
2021-10-03 01:05:14 +0700

    Tugas5

---
+++ Pertemuan 5/belajaroperator.py
@@ -0,0 +1,13 @@
+a = input("Masukkan nilai A = ")
+b = input("Masukan nilai B = ")
+x = True
+print("A == B = " + str(a==b))
+print("A != B = " + str(a!=b))
+print("A > B = " + str(a>b))
+print("A < B = " + str(a<b))
+print("A >= B = " + str(a>=b))
+print("A <= B = " + str(a<=b))
+x = int(a) < 5 and int(b) > 4
+print("A > 5 and B > 4 = " + str(x))
+print("A > 5 and B > 4 = " + str(int(a) < 5 or int(b) > 4))
+print("A > 5 and B > 4 = " + str(not(int(a) < 5)))
